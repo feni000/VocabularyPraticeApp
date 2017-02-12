3dd3217ff054e169ecc9f5ce2ac544853aff2c52
feni000 <phoenixa27@gmail.com>
2017-02-12 12:11:45 +0100

    Added Architecture of Application

---
+++ VocabularyPracticeApp/VocabularyPracticeApp/View/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using VocabularyPracticeApp.DAL;
+
+namespace VocabularyPracticeApp.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Practice.Items.Clear();
+            Delete.Items.Clear();
+            string[] languageSetsFromDatabase = new string[4] { "a", "b", "c", "d" };
+            foreach (string language in languageSetsFromDatabase)
+            {
+                MenuItem item = new MenuItem();
+                item.Header = language;
+                //item.Click += new RoutedEventHandler(File_Language_Click);
+                Practice.Items.Add(item);
+                MenuItem itemD = new MenuItem();
+                itemD.Header = language;
+                Delete.Items.Add(itemD);
+            }
+        }
+    }
+}
